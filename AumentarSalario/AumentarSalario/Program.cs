1cc849e3291a476aeb30a51895b136e445696548
zammsara <jsavalos@uamv.edu.ni>
2024-08-21 17:19:42 -0600

    quinto ejercicio

---
+++ AumentarSalario/AumentarSalario/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Salario
+{
+    class Salarios
+    {
+        static void Main(string[] args)
+        {
+            string nombre, cargo;
+            double salario, nuevoSalario;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese su cargo: ");
+            cargo = Console.ReadLine();
+            Console.Write("Ingrese su salario: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+
+            nuevoSalario = salario + (0.10 * salario);
+
+            Console.WriteLine($"El salario de {nombre}, con el cargo de {cargo}, es: {nuevoSalario}");
+
+
+
+
+        }
+    }
+}
