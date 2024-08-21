448688d3245f37c96db338061139fd4733ed5625
zammsara <jsavalos@uamv.edu.ni>
2024-08-21 15:55:40 -0600

    tercer ejercicio

---
+++ Sumar/Sumar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace suma
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int num1, num2, total;
+            Console.Write("Ingrese el primer numero: ");
+            //pq cuando el usuario ingresa un string 
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el segundo numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            total = num1 + num2;
+
+            //Console.WriteLine("La suma es: " + total);
+            Console.WriteLine("La suma entre {0} y {1} es {2}", num1, num2, total);
+        }
+
+    }
+}
