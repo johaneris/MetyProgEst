900b1069505ab0ffe46c3e4c3381fd66ffaa459a
zammsara <jsavalos@uamv.edu.ni>
2024-08-21 15:14:03 -0600

    segundo ejercicio

---
+++ Saludar/Saludar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace saludar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Holiiii" + " " + nombre);
+
+
+        }
+
+    }
+}
