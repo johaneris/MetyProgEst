c38acda2e1c00d6ef3f7f45b16aa51ba3c46d36c
zammsara <jsavalos@uamv.edu.ni>
2024-08-21 16:38:37 -0600

    cuarto ejercicio

---
+++ AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        const double pi = 3.14;
+        double area, radio;
+
+        Console.Write("Ingrese el radio: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+
+
+        area = pi * (radio * radio);
+
+
+        Console.WriteLine($"EL radio delcirculo es: {area}");
+
+
+        
+
+        }
+    }
+}
