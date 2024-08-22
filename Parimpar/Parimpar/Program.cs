ae474ca895dab7094373681182c63ad4c05b379c
zammsara <jsavalos@uamv.edu.ni>
2024-08-21 18:59:35 -0600

    sexto ejercicio

---
+++ Parimpar/Parimpar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace parImpar
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Ingrese un numero: ");
+            numero = Convert.ToInt32(Console.ReadLine() ??"");
+
+            if(numero % 2 == 0)
+            {
+                Console.WriteLine("Es un numero Par :33");
+            }
+            else
+            {
+                Console.WriteLine("Es un numero impar :(");
+
+
+            }
+        }
+
+    }
+}
