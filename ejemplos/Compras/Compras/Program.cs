c4f511c0623e77ebd2d95b12eaf89444d856f751
zammsara <jsavalos@uamv.edu.ni>
2024-08-23 08:41:59 -0600

    ejercico de la clase del 23 de agosto

---
+++ ejemplos/Compras/Compras/Program.cs
@@ -0,0 +1,45 @@
+﻿using Compras.clases;
+
+namespace Compras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo 1");
+
+            //crear nuestro primer objeto
+
+            Factura fact = new Factura();
+
+            //crear variables de entra
+
+            string producto = null; //todas las variables inician con un valor, asigna null para que este vacio
+            double precio = 0, monto;
+            int cantidad = 0;
+            Console.Write("Producto: ");
+            producto = Console.ReadLine();
+            Console.Write("Precio: "); //para espacio basicamente
+            precio = double.Parse(Console.ReadLine()); // es lo mismo que el convert, metodo para convertir 
+            Console.Write("Cantidad: ");
+            cantidad = int.Parse(Console.ReadLine());
+
+            fact.Producto = producto;
+            fact.Precio = precio;
+            fact.Cantidad = cantidad;
+
+
+            monto = fact.calcularMonto();
+
+
+
+
+
+            Console.WriteLine($"{producto}"); //intercola
+            Console.WriteLine($"{precio}"); //intercola
+            Console.WriteLine($"{cantidad}"); //intercola
+            Console.WriteLine($"{monto}"); //intercola
+            
+        }
+    }
+}
---
+++ ejemplos/Compras/Compras/clases/Factura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Compras.clases
+{
+    internal class Factura
+    {
+        public string Producto {  get; set; } // producto es el atributo, son como caracteristicas y get set son los metodos
+        public double Precio { get; set; } //get obtiene el valor y set lo asigna, metodos, hay atributos natos vv 
+        public int Cantidad { get; set; }
+        public double calcularMonto() //sin parametros :0
+        {
+            return this.Precio * this.Cantidad;
+        }
+
+        //contructor es la forma en la que creamos nuevos objetos, facilita la creacion de nuevos objetos 
+        public Factura()
+        {
+
+        }
+
+    }
+}
