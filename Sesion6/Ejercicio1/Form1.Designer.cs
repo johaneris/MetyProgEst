a3839077ef1543c9c4cb02892ee81c80f5010c0f
zammsara <jsavalos@uamv.edu.ni>
2024-09-06 08:50:03 -0600

    ejercicio del 9 de agosto

---
+++ Sesion6/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.cmbNombres = new System.Windows.Forms.ComboBox();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.cmdNombres2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.tbNombre.Location = new System.Drawing.Point(163, 28);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(353, 30);
+            this.tbNombre.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnAgregar.Image = global::Ejercicio1.Properties.Resources.add;
+            this.btnAgregar.Location = new System.Drawing.Point(522, 22);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(58, 42);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // cmbNombres
+            // 
+            this.cmbNombres.FormattingEnabled = true;
+            this.cmbNombres.Location = new System.Drawing.Point(80, 113);
+            this.cmbNombres.Name = "cmbNombres";
+            this.cmbNombres.Size = new System.Drawing.Size(500, 33);
+            this.cmbNombres.TabIndex = 3;
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Location = new System.Drawing.Point(80, 196);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(500, 58);
+            this.btnCopiar.TabIndex = 4;
+            this.btnCopiar.Text = "Copiar todos los elentos";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // cmdNombres2
+            // 
+            this.cmdNombres2.FormattingEnabled = true;
+            this.cmdNombres2.Location = new System.Drawing.Point(80, 274);
+            this.cmdNombres2.Name = "cmdNombres2";
+            this.cmdNombres2.Size = new System.Drawing.Size(500, 33);
+            this.cmdNombres2.TabIndex = 5;
+            this.cmdNombres2.SelectedIndexChanged += new System.EventHandler(this.cmdNombres2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 573);
+            this.Controls.Add(this.cmdNombres2);
+            this.Controls.Add(this.btnCopiar);
+            this.Controls.Add(this.cmbNombres);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ComboBox cmbNombres;
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.ComboBox cmdNombres2;
+    }
+}
+
---
+++ Sesion6/Ejercicio1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            agregar();
+            
+        }
+
+        public void agregar()
+        {
+            string nombre = tbNombre.Text.Trim(); //borra espacios al inicio y al final
+            if (string.IsNullOrEmpty(nombre))
+            {
+                MessageBox.Show("no puede quedar vacio el nombre");
+                tbNombre = null;
+                return;
+
+            }
+            cmbNombres.Items.Add(nombre);
+            tbNombre.Text = "";
+            tbNombre.Focus();
+
+        }
+
+        private void cmdNombres2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int cant = cmdNombres2.Items.Count;
+            for(int i = 0; i <cant; i++)
+            {
+                string nombre = cmbNombres.Items[i].ToString();
+                cmdNombres2.Items.Add(nombre);
+            }
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //public bool validarCampo(TextBox tb)
+        //{
+        //    if (tb.Text.Length == 0) return true;
+        //    return false;
+
+        //}
+
+        //public bool Validar(string texto)
+        //{
+        //    if (texto.Length == 0) return true;
+        //    return false;
+
+        //}
+    }
+}
